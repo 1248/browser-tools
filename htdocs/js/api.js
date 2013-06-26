549a9ebec2e123e02672dbbf4975c5c0fd3be3ba
Toby Jaffey <toby@1248.io>
2013-06-26 14:15:25 +0100

    Refactor fetch and populate to library

--- htdocs/js/api.js
+++ htdocs/js/api.js
@@ -5,3 +5,39 @@ function log(msg) {
     log.scrollTo('100%');
 }
 
+
+function populateExamples() {
+    $("#examples").append(new Option("Select example URL", ""));
+
+    $.ajax({
+        type: 'GET',
+        url: '/listexamples',
+        dataType: 'json',
+        success: function(body, textStatus, xhr) {
+            for (var i=0;i<body.length;i++)
+                $("#examples").append(new Option(body[i], body[i]));
+        },
+        error: function() {
+            log("Error listing examples");
+        }
+    });
+}
+
+function fetch(url, cb) {
+    log('-> GET ' + url);
+    $.ajax({
+        type: 'GET',
+        url: '/fetch?url='+encodeURI(url),
+        dataType: 'json',
+        success: function(body, textStatus, xhr) {
+            log('<- ' + xhr.status + ' ' + xhr.statusText);
+            cb(null, body);
+        },
+        error: function() {
+            log("Error fetching "+url);
+        }
+    });
+}
+
+
+
