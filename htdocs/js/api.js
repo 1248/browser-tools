854b02330a96acce2f25681fed5a197f586ddfc4
Toby Jaffey <toby@1248.io>
2013-06-26 14:28:52 +0100

    Refactor out ArrNoDupe()

--- htdocs/js/api.js
+++ htdocs/js/api.js
@@ -51,3 +51,13 @@ function storeFact(o) {
     facts.push(o);
 }
 
+function ArrNoDupe(a) {
+    var temp = {};
+    for (var i = 0; i < a.length; i++)
+        temp[a[i]] = true;
+    var r = [];
+    for (var k in temp)
+        r.push(k);
+    return r;
+}
+
